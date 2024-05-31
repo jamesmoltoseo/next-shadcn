545406afc36408250dda9847a508c77b82aa065b
jamesmoltoseo <james@moltoseo.co.uk>
2024-05-31 21:29:01 +0100

    First commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sports Bet | Aposta Esportivas Online, Brasil",
+  description: "O principal portal de apostas esportivas no Brasil.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
