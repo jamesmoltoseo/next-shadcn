28899df4529f9322c28b18cfaf5d93c4103f2c19
jamesmoltoseo <james@moltoseo.co.uk>
2024-06-01 04:20:10 +0100

    new banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -163,9 +163,18 @@ export default function Home() {
         </div>
       </header>
       <section>
-        <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
-          <div className="flex w-full mx-auto text-left">
-            <div className="relative inline-flex items-center mx-auto align-middle">
+        <div className="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 lg:py-24">
+        <Image
+              src="/img/banners/homepage/aposta-esportiva.webp"
+              alt="Aposta Esportiva"
+              layout="fill"
+              objectFit="cover"
+              priority
+            /><div className="flex w-full mx-auto text-left">
+            <div className="relative flex items-center justify-end">
+            
+           
+         
               <div className="text-center">
                 <h1 className="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-neutral-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
                   Long headline to turn <br className="hidden lg:block" /> your
